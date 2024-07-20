9fed82f8e7b5767bf72074324107aa9583450be8
Abhinav Midha <21ucs003@lnmiit.ac.in>
2024-07-20 05:45:25 +0530

    chatshat-frontend

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,172 @@
+import {
+  AppBar,
+  Backdrop,
+  Badge,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { orange } from "../../constants/color";
+import React, { Suspense, useState, lazy } from "react";
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  Add as AddIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Notifications as NotificationsIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../constants/config";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { userNotExists } from "../../redux/reducers/auth";
+import {
+  setIsMobile,
+  setIsNewGroup,
+  setIsNotification,
+  setIsSearch,
+} from "../../redux/reducers/misc";
+import { resetNotificationCount } from "../../redux/reducers/chat";
+
+const SearchDialog = lazy(() => import("../specific/Search"));
+const NotificationDialog = lazy(() => import("../specific/Notifications"));
+const NewGroupDialog = lazy(() => import("../specific/NewGroup"));
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { isSearch, isNotification, isNewGroup } = useSelector(
+    (state) => state.misc
+  );
+  const { notificationCount } = useSelector((state) => state.chat);
+
+  const handleMobile = () => {
+    dispatch(setIsMobile(true));
+  };
+
+  const openSearch = () => {
+    dispatch(setIsSearch(true));
+  };
+
+  const openNewGroup = () => {
+    dispatch(setIsNewGroup(true));
+  };
+
+  const navigateToGroup = () => navigate("/groups");
+
+  const openNotification = () => {
+    dispatch(setIsNotification(true));
+    dispatch(resetNotificationCount());
+  };
+  const logoutHandler = async () => {
+    try {
+      const { data } = await axios.get(`${server}/api/v1/user/logout`, {
+        withCredentials: true,
+      });
+      dispatch(userNotExists());
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+    }
+  };
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar position="static" sx={{ bgcolor: orange }}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              𝓒𝓱𝓪𝓽𝓢𝓱𝓪𝓽
+            </Typography>
+
+            <Box sx={{ display: { xs: "block", sm: "none" } }} height={"4rem"}>
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+
+            {/* ye wala box sara space lelega */}
+            <Box sx={{ flexGrow: 1 }}></Box>
+
+            <Box>
+              <IconBtn
+                title={"Search"}
+                icon={<SearchIcon />}
+                onclick={openSearch}
+              />
+
+              <IconBtn
+                title={"New Group"}
+                icon={<AddIcon />}
+                onclick={openNewGroup}
+              />
+
+              <IconBtn
+                title={"Manage Groups"}
+                icon={<GroupIcon />}
+                onclick={navigateToGroup}
+              />
+
+              <IconBtn
+                title={"Notifications"}
+                icon={<NotificationsIcon />}
+                onclick={openNotification}
+                value={notificationCount}
+              />
+
+              <IconBtn
+                title={"Logout"}
+                icon={<LogoutIcon />}
+                onclick={logoutHandler}
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      {isSearch && (
+        <Suspense fallback={<Backdrop open />}>
+          <SearchDialog />
+        </Suspense>
+      )}
+
+      {isNotification && (
+        <Suspense fallback={<Backdrop open />}>
+          <NotificationDialog />
+        </Suspense>
+      )}
+
+      {isNewGroup && (
+        <Suspense fallback={<Backdrop open />}>
+          <NewGroupDialog />
+        </Suspense>
+      )}
+    </>
+  );
+};
+
+const IconBtn = ({ title, icon, onclick, value }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onclick}>
+        {value ? (
+          <Badge badgeContent={value} color="error">
+            {icon}
+          </Badge>
+        ) : (
+          icon
+        )}
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default Header;
---
+++ src/pages/admin/AdminLogin.jsx
@@ -0,0 +1,97 @@
+import {
+  Container,
+  Paper,
+  TextField,
+  Typography,
+  Button,
+  Stack,
+  Avatar,
+  IconButton,
+} from "@mui/material";
+
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import { bgGradient } from "../../constants/color";
+import { useInputValidation } from "6pp";
+import { Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { adminLogin, getAdmin } from "../../redux/thunks/admin";
+import { useEffect } from "react";
+
+const AdminLogin = () => {
+  const { isAdmin } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const secretKey = useInputValidation("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(adminLogin(secretKey.value));
+  };
+
+  useEffect(() => {
+    dispatch(getAdmin());
+  }, [dispatch]);
+
+  if (isAdmin) return <Navigate to="/admin/dashboard" />;
+
+  return (
+    <div
+      style={{
+        backgroundImage: bgGradient,
+      }}
+    >
+      <Container
+        component={"main"}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5">Admin Login</Typography>
+          <form
+            style={{
+              width: "100%",
+              marginTop: "1rem",
+            }}
+            onSubmit={submitHandler}
+          >
+            <TextField
+              required
+              fullWidth
+              label="Secret Key"
+              type="password"
+              margin="normal"
+              variant="outlined"
+              value={secretKey.value}
+              onChange={secretKey.changeHandler}
+            />
+
+            <Button
+              sx={{ marginTop: "1rem" }}
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default AdminLogin;
